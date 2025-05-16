533c21bbaafb82c820052972ad43639b1f26fdfd
leekyungho <epsoe@naver.com>
2025-05-16 15:37:17 +0900

    shadcn Card 설치

--- components/auth/FormCard.tsx
+++ components/auth/FormCard.tsx
@@ -1,3 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
 type Props = {
   title: string;
   footer: { label: string; href: string };
@@ -5,5 +14,18 @@ type Props = {
 };
 
 export function FormCard({ title, footer, children }: Props) {
-  return <></>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }
